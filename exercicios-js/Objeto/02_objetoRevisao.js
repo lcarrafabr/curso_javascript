dd48f4ed6032d7cd225edb981ef1a4a68b3dccd3
lcarrafa.br@gmail.com <lcarrafa.br@gmail.com>
2020-01-21 07:16:29 -0300

    Revisão sobre Objeto

--- exercicios-js/Objeto/02_objetoRevisao.js
+++ exercicios-js/Objeto/02_objetoRevisao.js
@@ -7,4 +7,46 @@ produto.preco = 220
 
 console.log(produto)
 delete produto.preco
-delete produto.['Marca do produto']
+delete produto['Marca do produto']
+
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Luciano',
+        idade: 34,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 10000,
+            bairro: 'PQP',
+            cidade: 'São Paulo',
+            estado: 'SP'
+        }
+    },
+    condutores: [{
+        nome: 'Debora',
+        idade: 30
+    }, {
+        nome: 'Dolly',
+        idade: 18
+    }],
+    calcularValorSeguro: function(){
+        //...
+    }
+}
+
+//Posso acessaros atributos a aprtir da aniotação . (ponto)
+carro.proprietario.endereco.numero = 100
+
+//posso acessar atraves dos [ ] mesmo que não os tenha definido.
+carro ['proprietario'] ['endereco'] ['logradouro'] = 'Av gigante'
+
+console.log(carro)
+
+delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+
+console.log(carro)
